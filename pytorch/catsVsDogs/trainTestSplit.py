395a6bbce6c601476a0681c80257449688a00660
Aman Satyawali <amansatyawali@gmail.com>
2022-01-18 22:35:30 +0530

    Added condition in the train test split script so that it only runs the first time

--- pytorch/catsVsDogs/trainTestSplit.py
+++ pytorch/catsVsDogs/trainTestSplit.py
@@ -1,12 +1,22 @@
 import os
 import numpy as np
 import shutil
+import sys
 
 TEST_RATIO = 0.2
 
 TRAIN_DIR = 'data/train'
 TEST_DIR = 'data/test'
+TRAIN_DATA_SIZE = 25000
 
+
+if os.path.exists(TRAIN_DIR) and len(os.listdir(TRAIN_DIR)) != TRAIN_DATA_SIZE :
+    print('number of training data and the gived input size does not match')
+    sys.exit()
+
+if os.path.exists(TEST_DIR) and len(os.listdir(TEST_DIR)) :
+    print('Test directory already has files, please check')
+    sys.exit()
 
 dataSetFileNames = os.listdir(TRAIN_DIR) # get the names of all the files in the train folder so that it is easier to move data by name
 dataSetLen = len(dataSetFileNames)
@@ -26,3 +36,5 @@ for i in testIdx :
     newFilePath = TEST_DIR + '/' + dataSetFileNames[i]
     shutil.copyfile(currFilePath, newFilePath)        #Copy each file with the test index to the test folder
     os.remove(currFilePath)                           #Delete the file from the train directory
+
+print('transfer complete')
